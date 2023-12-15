379e267b154d85e405f31748ac33e86749b4c91d
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2023-12-15 15:24:40 +0100

    Update build config (#2)
    * update wrapper
    
    * update plugins
    
    * use non final res idea for performance
    
    * update build config and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "io.didomi.sampleappcompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "io.didomi.sampleappcompose"
 
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
 
         versionCode = 1
         versionName = "1.0"
@@ -42,14 +42,14 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
-    packagingOptions {
+    packaging {
         resources {
             excludes.add("/META-INF/{AL2.0,LGPL2.1}")
         }
@@ -57,20 +57,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.4.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
-    implementation("com.google.android.gms:play-services-ads:21.5.0")
+    implementation("com.google.android.gms:play-services-ads:22.6.0")
 
-    implementation("io.didomi.sdk:android:1.86.3")
+    implementation("io.didomi.sdk:android:1.87.0")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
-
-    testImplementation("junit:junit:4.13.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
 }
