7d7a417f2829efc1b93ed1fbed626eef13f5f0ec
Nicolas Chaix <nicolas.chaix@didomi.io>
2024-09-04 10:46:37 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,18 +57,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
-    implementation("com.google.android.gms:play-services-ads:22.6.0")
+    implementation("com.google.android.gms:play-services-ads:23.3.0")
 
-    implementation("io.didomi.sdk:android:2.9.0")
+    implementation("io.didomi.sdk:android:2.11.0")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
