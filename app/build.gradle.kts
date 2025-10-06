736f879304af05de16267078317479a9ce99aaea
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2025-10-06 15:47:36 +0200

    Update build config (#6)
    * chore(gradle): update gradle wrapper
    
    * refactor(didomi): replace deprecated
    
    * refactor(gradle): update AGP, dependencies and use Compose plugin for Kotlin 2.x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,25 +3,21 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("plugin.compose")
 }
 
 android {
     namespace = "io.didomi.sampleappcompose"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.didomi.sampleappcompose"
 
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 23
+        targetSdk = 36
 
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -37,16 +33,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     packaging {
@@ -57,18 +51,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation("androidx.compose.material:material:1.6.8")
-    implementation("androidx.compose.ui:ui:1.6.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.compose.material:material:1.9.2")
+    implementation("androidx.compose.ui:ui:1.9.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.2")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
 
-    implementation("com.google.android.gms:play-services-ads:23.3.0")
+    implementation("com.google.android.gms:play-services-ads:24.6.0")
 
-    implementation("io.didomi.sdk:android:2.30.0")
+    implementation("io.didomi.sdk:android:2.31.0")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.2")
 }
